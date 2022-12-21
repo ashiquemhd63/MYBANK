e2f1bc2ba562343eed3caae3455fd31a5fbe30f8
Raj Kishore yadav <raj.yadav@experionglobal.com>
2022-12-21 19:23:48 +0530

    Captcha Done
    I done  Captcha in login page .This is running and working successfully. Please review.

--- Client/my-bank/src/Components/Public/Login.js
+++ Client/my-bank/src/Components/Public/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React,useState} from 'react';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 import AuthService from "../../PublicServices/authService";
